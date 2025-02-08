748ba1a94a681f131d01bde3d0aeacdf7248ac3e
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-02-08 22:05:49 +0530

    Update datatypes.go

--- practices/go_scripts/datatypes.go
+++ practices/go_scripts/datatypes.go
@@ -18,6 +18,20 @@ func main() {
 	d := 88.90
 	fmt.Println("Given input of 'd' value is:", d)
 	fmt.Println("Type of 'c' is:", reflect.TypeOf(d))
+	f := "How is your health"
+	g := "John"
+	fmt.Println("Combining variables like 'f' and 'g':", f, g, "?")
+	fmt.Println("Combining variables like 'f' and 'g':", f, " ", g, "?")
+	h := "Kodekloud"
+	i := "GO language program instructor"
+	fmt.Print("One of the best online learning platform is: ", h, "\n")
+	fmt.Print("Priyanka is one of the best ", "", i, "\n")
+	j := "Kodekloud"
+	k := "GO language program instructor"
+	fmt.Println("One of the best online learning platform is:", j)
+	fmt.Println("Priyanka is one of the best", k)
 }
 
-// reflect.TypeOf(variable) --> returns the type of the variable, which is then printed out.
+// reflect.TypeOf(variable) returns the type of the variable.
+// "\n" is used for a manual newline when using fmt.Print.
+// fmt.Println automatically includes a newline, so no need to add "\n".

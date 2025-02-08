357683a4df8b2b4ff1d97e6fd50a767f6d719a84
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-02-08 21:08:14 +0530

    updated datatype

--- practices/go_scripts/datatypes.go
+++ practices/go_scripts/datatypes.go
@@ -7,13 +7,13 @@ import (
 
 func main() {
 	a := "India is my country"
-	fmt.Println(a)
+	fmt.Println("Given input of 'a' value is:", a)
 	fmt.Println("Type of 'a' is:", reflect.TypeOf(a))
 	b := 76534543
-	fmt.Println(b)
+	fmt.Println("Given input of 'b' value is:", b)
 	fmt.Println("Type of 'b' is:", reflect.TypeOf(a))
 	c := true
-	fmt.Println(c)
+	fmt.Println("Given input of 'c' value is:", c)
 	fmt.Println("Type of 'c' is:", reflect.TypeOf(c))
 }
 

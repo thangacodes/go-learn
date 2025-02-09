2371fcf8c3ac0319ddb17f0fc88cc3722c2f91e5
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-02-09 11:00:54 +0530

    Update findout_variables_type_method.go

--- practices/go_scripts/findout_variables_type_method.go
+++ practices/go_scripts/findout_variables_type_method.go
@@ -19,15 +19,14 @@ func main() {
 	fmt.Println("Sleep for '1' second..")
 	time.Sleep(1 * time.Second)
 	fmt.Println("Finding variable data types using the type-2 method..")
-	fmt.Printf("variable age = %v is datatype of %s \n", age, reflect.TypeOf(age).Name())
-	fmt.Printf("variable name = %v is datatype of %s \n", name, reflect.TypeOf(name).Name())
-	fmt.Printf("variable ismarried = %v is datatype of %s \n", ismarried, reflect.TypeOf(ismarried).Name())
-	fmt.Printf("variable height = %v is datatype of %s \n", height, reflect.TypeOf(height).Name())
+	fmt.Printf("variable age = %v is datatype of %v \n", age, reflect.TypeOf(age))
+	fmt.Printf("variable name = %v is datatype of %v \n", name, reflect.TypeOf(name))
+	fmt.Printf("variable ismarried = %v is datatype of %v \n", ismarried, reflect.TypeOf(ismarried))
+	fmt.Printf("variable height = %v is datatype of %v \n", height, reflect.TypeOf(height))
 }
 
-//Note:
-   // The %v is a format specifier used in fmt.Printf in Go. It is used to print the value of a variable in a default format.
-  // The %s is another format specifier in fmt.Printf, but this one is used specifically to print a string.
- // The 'reflect package' allows you to inspect types at runtime.
-     // reflect.TypeOf(variable) returns the reflect.Type of the variable
-    //  .Name() is a method that returns the name of the type (as a string)
+# Note:
+   # The %v is a format specifier used in fmt.Printf in Go. It is used to print the value of a variable in a default format.
+   # The %s is another format specifier in fmt.Printf, but this one is used specifically to print a string.
+   # The 'reflect package' allows you to inspect types at runtime.
+   # reflect.TypeOf(variable) returns the reflect.Type of the variable

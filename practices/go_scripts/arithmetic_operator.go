e9d6c37c95f9a7a9b36ec9d50823e891f73ab5e5
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-02-12 22:34:03 +0530

    updates on arithmetic Operator

--- practices/go_scripts/arithmetic_operator.go
+++ practices/go_scripts/arithmetic_operator.go
@@ -7,42 +7,42 @@ import (
 
 func main() {
 	fmt.Println("These are the common Arithmetic Operators :\n")
-	fmt.Println("addition (+)\nsubtraction (-)\nmultiplication (*)\ndivision (/)\nmodulus (%)\nincrement (++)\ndecrement (--)")
+	fmt.Println("addition operator(+)\nsubtraction operator (-)\nmultiplication operator(*)\ndivision operator(/)\nmodulus operator(%)\nincrement operator(++)\ndecrement operator(--)")
 	fmt.Println("")
 	time.Sleep(1 * time.Second)
-	fmt.Println("'addition operator(+)' example")
+	fmt.Println("'addition(+) operator' example")
 	var a, b string = "foo", "bar"
 	fmt.Println(a + b)
 	time.Sleep(1 * time.Second)
 	fmt.Println("")
-	fmt.Println("'subtraction operator(-)' example")
+	fmt.Println("'subtraction(-) operator' example")
 	var c, d float64 = 200.20, 150.24
 	fmt.Printf("%.4f", c-d)
 	fmt.Println("")
 	time.Sleep(1 * time.Second)
 	fmt.Println("")
-	fmt.Println("'Multiplication operator(*)' example")
+	fmt.Println("'Multiplication(*) operator' example")
 	var e, f int = 30, 2
 	fmt.Println(e * f)
 	time.Sleep(1 * time.Second)
 	fmt.Println("")
-	fmt.Println("'Division operator(/)' example")
+	fmt.Println("'Division(/) operator' example")
 	var g, h int = 100, 2
 	fmt.Println(g * h)
 	fmt.Println("")
 	time.Sleep(1 * time.Second)
-	fmt.Println("'Modulus operator(%)' example")
+	fmt.Println("'Modulus(%) operator' example")
 	var i, j int = 24, 3
 	fmt.Println(i % j)
 	fmt.Println("")
 	time.Sleep(1 * time.Second)
-	fmt.Println("'Increment operator(++)' example")
+	fmt.Println("'Increment(++) operator' example")
 	var k int = 2
 	k++
 	fmt.Println(k)
 	fmt.Println()
 	time.Sleep(1 * time.Second)
-	fmt.Println("'Decrement operator(--)' example")
+	fmt.Println("'Decrement(--) operator' example")
 	var l int = 2
 	l--
 	fmt.Println(l)
